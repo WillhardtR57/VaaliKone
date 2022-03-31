8c769f80b2e240ff1b7cf9c5df16e89c13fc8884
WillhardtR57 <willhardt21001@student.hamk.fi>
2022-03-31 20:38:54 +0300

    Update HelloAppEngine.java

--- VaaliKoneSevrlet/src/main/java/app/HelloAppEngine.java
+++ VaaliKoneSevrlet/src/main/java/app/HelloAppEngine.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import app.dao.Dao;
+import app.Dao;
 @WebServlet(
     name = "HelloAppEngine",
     urlPatterns = {"/hello"}
